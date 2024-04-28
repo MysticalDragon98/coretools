0447defd624969ccdb5502d4e0a4b9cc3439094b
CamiloTDex <camilotd1999@gmail.com>
2024-04-28 14:25:03 -0500

    Refactor admin user creation and initialization in index.sh

--- build/index.sh
+++ build/index.sh
@@ -167,12 +167,12 @@ exists_user () {
 ADMIN_USER=admin
 
 init_admin_user () {
-    if $(exists_user $ADMIN_USER); then
-        print ${SYMBOL_OK} " Admin user is setup"
+    if $(exists_user $ADMIN_USER) -eq "true"; then
+        print ${SYMBOL_OK} "Admin user is setup"
     else
         print "Creating admin user..."
         create_user $ADMIN_USER
-        print ${SYMBOL_OK} " Admin user created."
+        print ${SYMBOL_OK} "Admin user created."
     fi
 }
 
