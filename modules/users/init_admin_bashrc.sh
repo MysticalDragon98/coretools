04bfcd19da31301900ce79598005803ab7da1b64
CamiloTDex <camilotd1999@gmail.com>
2024-04-28 15:15:09 -0500

    Refactor BASE_PATH variable initialization in init_admin_bashrc.sh

--- modules/users/init_admin_bashrc.sh
+++ modules/users/init_admin_bashrc.sh
@@ -1,12 +1,12 @@
 init_admin_bashrc () {
     local user=$1
 
-    if [ -f "$user/.bashrc" ]; then
+    if [ -f "$BASE_PATH/.bashrc" ]; then
         print ${SYMBOL_OK} "Admin user .bashrc is setup"
     else
         print "Setting up admin user .bashrc..."
         
-        sudo curl -s $FILE_BASHRC > /home/$ADMIN_USER/.bashrc
+        sudo curl -s $FILE_BASHRC > $BASE_PATH/.bashrc
 
         print ${SYMBOL_OK} "Admin user .bashrc setup."
     fi
