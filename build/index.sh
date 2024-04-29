3e5c45d4575140c19bde6e484c3d6fd0d857c1c1
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:23:32 -0500

    Fix missing newline at end of file in ensure_bash_after_login_script

--- build/index.sh
+++ build/index.sh
@@ -392,7 +392,7 @@ ensure_bash_after_login_script () {
     local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
 
     if [ -z "$bash_script_content" ]; then
-        sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee $bash_script > /dev/null
+        sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
 }
 
--- lib/modules/bash/ensure_bash_after_login_script.sh
+++ lib/modules/bash/ensure_bash_after_login_script.sh
@@ -3,6 +3,6 @@ ensure_bash_after_login_script () {
     local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
 
     if [ -z "$bash_script_content" ]; then
-        sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee $bash_script > /dev/null
+        sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
 }
