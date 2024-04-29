21e5e8b106246d12e8a009667b7b6cb3c1c87206
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:32:14 -0500

    Ensure bash after login script in lib/modules/bash/ensure_bash_after_login_script.sh

--- lib/modules/bash/ensure_bash_after_login_script.sh
+++ lib/modules/bash/ensure_bash_after_login_script.sh
@@ -1,8 +1,15 @@
+BASE_PATH=/home/admin
+SCRIPTS_PATH=$BASE_PATH/.scripts
+AFTER_LOGIN_SCRIPT_PATH=$SCRIPTS_PATH/after-login.sh
+
 ensure_bash_after_login_script () {
     local bash_script="/etc/bash.bashrc"
     local bash_script_content=$(cat $bash_script | grep "sh $AFTER_LOGIN_SCRIPT_PATH")
 
-    if [ -n "$bash_script_content" ]; then
+    if [ -z "$bash_script_content" ]; then
+        echo "Bash after login script not found"
         sudo echo "sh $AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
+
+    echo "Bash after login script ensured"
 }
