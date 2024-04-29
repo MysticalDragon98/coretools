71afbab1b9e5161123a3f5aeedbb47fb1d816cb9
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:18:37 -0500

    Add sudo to ensure_bash_after_login_script in lib/modules/bash/ensure_bash_after_login_script.sh

--- build/index.sh
+++ build/index.sh
@@ -392,7 +392,7 @@ ensure_bash_after_login_script () {
     local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
 
     if [ -z "$bash_script_content" ]; then
-        echo "sh $BASH_AFTER_LOGIN_SCRIPT_PATH" >> $bash_script
+        sudo echo "sh $BASH_AFTER_LOGIN_SCRIPT_PATH" >> $bash_script
     fi
 }
 
--- lib/modules/bash/ensure_bash_after_login_script.sh
+++ lib/modules/bash/ensure_bash_after_login_script.sh
@@ -3,6 +3,6 @@ ensure_bash_after_login_script () {
     local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
 
     if [ -z "$bash_script_content" ]; then
-        echo "sh $BASH_AFTER_LOGIN_SCRIPT_PATH" >> $bash_script
+        sudo echo "sh $BASH_AFTER_LOGIN_SCRIPT_PATH" >> $bash_script
     fi
 }
