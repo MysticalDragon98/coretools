2932923b86393650fd82f9097e4a6aaadb7553be
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:36:30 -0500

    Fix conditional logic in ensure_bash_after_login_script

--- build/index.sh
+++ build/index.sh
@@ -391,7 +391,7 @@ ensure_bash_after_login_script () {
     local bash_script="/etc/bash.bashrc"
     local bash_script_content=$(cat $bash_script | grep "sh $AFTER_LOGIN_SCRIPT_PATH")
 
-    if [ -n "$bash_script_content" ]; then
+    if [ -z "$bash_script_content" ]; then
         sudo echo "sh $AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
 }
