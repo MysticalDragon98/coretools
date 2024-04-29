6db1df38f19d56efdd9195db8f1d9bd2864656fe
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:27:21 -0500

    Fix conditional logic in ensure_bash_after_login_script

--- lib/modules/bash/ensure_bash_after_login_script.sh
+++ lib/modules/bash/ensure_bash_after_login_script.sh
@@ -2,7 +2,7 @@ ensure_bash_after_login_script () {
     local bash_script="/etc/bash.bashrc"
     local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
 
-    if [ -z "$bash_script_content" ]; then
+    if [ -n "$bash_script_content" ]; then
         sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
 }
