367b8c230e7577713a34c759f629c4c06f154f21
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:28:19 -0500

    Update variable name in ensure_bash_after_login_script

--- lib/modules/bash/ensure_bash_after_login_script.sh
+++ lib/modules/bash/ensure_bash_after_login_script.sh
@@ -1,8 +1,8 @@
 ensure_bash_after_login_script () {
     local bash_script="/etc/bash.bashrc"
-    local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
+    local bash_script_content=$(cat $bash_script | grep "sh $AFTER_LOGIN_SCRIPT_PATH")
 
     if [ -n "$bash_script_content" ]; then
-        sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
+        sudo echo "sh $AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
 }
