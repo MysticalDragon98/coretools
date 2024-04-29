0e855c4650b485bcc80d3ed0fe418f75fc810771
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:28:26 -0500

    Refactor variable name in ensure_bash_after_login_script

--- build/index.sh
+++ build/index.sh
@@ -389,10 +389,10 @@ verify_coretool () {
 
 ensure_bash_after_login_script () {
     local bash_script="/etc/bash.bashrc"
-    local bash_script_content=$(cat $bash_script | grep "sh $BASH_AFTER_LOGIN_SCRIPT_PATH")
+    local bash_script_content=$(cat $bash_script | grep "sh $AFTER_LOGIN_SCRIPT_PATH")
 
     if [ -n "$bash_script_content" ]; then
-        sudo echo "\nsh $BASH_AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
+        sudo echo "sh $AFTER_LOGIN_SCRIPT_PATH" | sudo tee -a $bash_script > /dev/null
     fi
 }
 
