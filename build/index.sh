71b6438a8fce121ea6c9f4a083371144a0260b19
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 21:33:28 -0500

    Refactor ownership assignment in install_scripts function

--- build/index.sh
+++ build/index.sh
@@ -423,7 +423,7 @@ is_scripts_installed () {
 
 install_scripts () {
     sudo git clone $SCRIPTS_REPO $SCRIPTS_PATH
-    chown -R $ADMIN_USER_VAR:$ADMIN_USER_VAR $SCRIPTS_PATH
+    own $SCRIPTS_PATH
     
     ensure_bash_after_login_script
 }
