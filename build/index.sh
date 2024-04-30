39d2b061d63d51c34559fa1c20b10e5633b34a02
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 21:32:24 -0500

    Update print statement in ensure_supervisor_admin_permissions.sh

--- build/index.sh
+++ build/index.sh
@@ -489,7 +489,7 @@ ensure_line () {
 }
 
 ensure_supervisor_admin_permissions () {
-    echo "Ensuring that $SUPERVISOR_SOCK_PATH belongs to the $ADMIN user"
+    print "Ensuring that $SUPERVISOR_SOCK_PATH belongs to the $ADMIN user"
     own $SUPERVISOR_SOCK_PATH
 }
 
--- lib/modules/supervisor/ensure_supervisor_admin_permissions.sh
+++ lib/modules/supervisor/ensure_supervisor_admin_permissions.sh
@@ -1,4 +1,4 @@
 ensure_supervisor_admin_permissions () {
-    echo "Ensuring that $SUPERVISOR_SOCK_PATH belongs to the $ADMIN user"
+    print "Ensuring that $SUPERVISOR_SOCK_PATH belongs to the $ADMIN user"
     own $SUPERVISOR_SOCK_PATH
 }
