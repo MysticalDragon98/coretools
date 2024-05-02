bab43f32bbcf78d277a0511fbee62641f941969a
CamiloTDex <camilotd1999@gmail.com>
2024-05-02 13:07:20 -0500

    Add installation of bin directory in install_coretools.sh

--- build/index.sh
+++ build/index.sh
@@ -450,6 +450,10 @@ install_coretools () {
     if ! $(is_scripts_installed) -eq "true"; then
         install_scripts
     fi
+
+    if ! $(is_bin_installed) -eq "true"; then
+        install_bin
+    fi
 }
 
 verify_coretools () {
--- lib/modules/coretools/install_coretools.sh
+++ lib/modules/coretools/install_coretools.sh
@@ -2,4 +2,8 @@ install_coretools () {
     if ! $(is_scripts_installed) -eq "true"; then
         install_scripts
     fi
+
+    if ! $(is_bin_installed) -eq "true"; then
+        install_bin
+    fi
 }
